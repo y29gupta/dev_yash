a7a28db55f3a16514cd74880ebb9e08689f0b9ce
Yash <yash@Shushants-MacBook-Air.local>
2023-08-30 12:02:57 +0530

    front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import React, { useMemo, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// import TodoList from './TodoList';
+
+const App = () => {
+ 
+ return(
+  <div>app</div>
+ )
+  
+};
 
 export default App;
